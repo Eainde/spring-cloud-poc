b85a2911bde78fed3bde40941fa8bda405ee3259
eainde <akshay.dipta@gmail.com>
2021-03-31 23:58:04 +0100

    reformatting

--- eainde-config-server/src/main/java/com/eainde/config/Application.java
+++ eainde-config-server/src/main/java/com/eainde/config/Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
