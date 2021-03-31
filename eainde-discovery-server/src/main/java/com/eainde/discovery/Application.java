16b19dabe569cc38b9f6b6bc024d56bd53b10dd8
eainde <akshay.dipta@gmail.com>
2021-03-31 23:53:23 +0100

    reformatting

--- eainde-discovery-server/src/main/java/com/eainde/discovery/Application.java
+++ eainde-discovery-server/src/main/java/com/eainde/discovery/Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class Application {
-    public static void main(String[] args){
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
